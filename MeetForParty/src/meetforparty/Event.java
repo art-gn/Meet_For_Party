59411d9293a411fc33164c7199d782237184543a
pc <arthurgandon@hotmail.fr>
2017-01-30 15:28:40 +0100

    LA PAUUUSE

--- MeetForParty/src/meetforparty/Event.java
+++ MeetForParty/src/meetforparty/Event.java
@@ -11,5 +11,38 @@ package meetforparty;
  * @author pc
  */
 public class Event {
+    private String Nom;
+    private String Date;
+    private String Jeu;
+
+    public Event(String Nom, String Date, String Jeu) {
+        this.Nom = Nom;
+        this.Date = Date;
+        this.Jeu = Jeu;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setNom(String Nom) {
+        this.Nom = Nom;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String Date) {
+        this.Date = Date;
+    }
+
+    public String getJeu() {
+        return Jeu;
+    }
+
+    public void setJeu(String Jeu) {
+        this.Jeu = Jeu;
+    }
     
 }
